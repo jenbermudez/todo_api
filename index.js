364a0342bd393e8e7d023f128db1fc48bc130548
Jennifer Bermudez <jenbermudez@gmail.com>
2018-11-27 17:36:06 -0800

    Account endpoint

--- index.js
+++ index.js
@@ -40,6 +40,55 @@ const todos = {
     }
 }
 
+const accounts = {
+    id: 0,
+    items: [],
+    create: function (firstName, lastName, email) {
+        const account = {
+            id: ++accounts.id,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+        }
+        for (let i = 0; i < accounts.items.length; i++) {
+            if (accounts.items[i].email === email) {
+                throw "email already exists"
+            }
+        }
+        accounts.items.push(account)
+        return account
+    },
+    getAll: function () {
+        return accounts.items
+    },
+    idCheck: function (idGiven) {
+        for (let i = 0; i < accounts.items.length; i++) {
+            if (accounts.items[i].id === idGiven) {
+                return i
+            }
+        }
+        throw "id does not exist"
+    },
+    getById: function (idGiven) {
+        const position = accounts.idCheck(idGiven)
+        return accounts.items[position]
+    },
+    update: function (idGiven, fName, lName, email2) {
+        const position = accounts.idCheck(idGiven);
+        const item = accounts.items[position];
+        item.firstName = fName;
+        item.lastName = lName;
+        item.email = email2;
+        return item
+    },
+    delete: function (idGiven) {
+        const position = accounts.idCheck(idGiven)
+        accounts.items.splice(position, 1)
+        return "deleted"
+    }
+}
+
+// *************TODOS******************
 // GET /todo => all todos
 app.get('/todo', (req, res) => res.json(todos.getAll()))
 
@@ -74,6 +123,53 @@ app.delete('/todo/:id', (req, res) => {
     const id = parseInt(req.params.id);
     try {
         res.json(todos.delete(id));
+    } catch (e) {
+        res.status(400).json({ error: e })
+    }
+})
+
+// ***************ACCOUNTS*****************
+// GET /account => all accounts
+app.get('/account', (req, res) => {
+    res.json(accounts.getAll())
+})
+
+// POST /account => create new account using ***body*** as message, return new account created with id inlcuded
+app.post('/account', (req, res) => {
+    const { firstName, lastName, email } = req.body;
+    try {
+        res.status(201).json(accounts.create(firstName, lastName, email))
+    } catch (e) {
+        res.status(400).json({ error: e })
+    }
+})
+
+// GET /account/<id> => return account with id = <id>
+app.get('/account/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        res.json(accounts.getById(id))
+    } catch (e) {
+        res.status(400).json({ error: e })
+    }
+})
+
+// PUT /account/<id> => update account with id = <id>
+app.put('/account/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { firstName, lastName, email } = req.body;
+    try {
+        res.json(accounts.update(id, firstName, lastName, email))
+    } catch (e) {
+        res.status(400).json({ error: e })
+    }
+})
+
+// DELETE /account/<id> => delete account with id = <id>
+app.delete('/account/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        res.json(accounts.delete(id));
     } catch (e) {
         res.status(400).json({ error: e })
     }
